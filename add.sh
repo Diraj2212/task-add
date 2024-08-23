3029cc09e5aa309c388b2fc3285f57771e374a6f
Chintu <neelamdheerajkumar1122@gmail.com>
2024-08-23 15:14:58 +0530

    msg1

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number: 40"
+
+# Ask user for second number
+echo "Enter second number: 20 "
+
+# Perform addition
+sum=$((40 + 20))
+
+# Display result
+echo "Sum: $sum"
